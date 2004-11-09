20c47e5081c2c9511509dba91f93dafa7b567b05
zeileis <zeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2004-11-09 14:04:02 +0000

    added Gabor's model.frame- und na.approx-stuff (R and Rd)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/zoo@45 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/R/na.approx.R
@@ -0,0 +1,24 @@
+na.approx <- function(y, x, FUN, na.rm = TRUE, ...) UseMethod("na.approx")
+
+# interpolates y using x which defaults to seq(along = y) if FUN
+# is missing or to FUN(time(y)) if not
+na.approx.default <- function(y, 
+	x = if (missing(FUN)) seq(along=time(y)) else FUN(time(y)), 
+	FUN, na.rm = TRUE, ...)
+{
+	# na.approx is based on post from r-help by 
+	# Sundar Dorai-Raj <sundar.dorai-raj@PDF.COM>
+	na.approx.0 <- function(y) {
+		na <- is.na(y)
+		if(all(!na)) return(y)
+		y[na] <- approx(x[!na], x[!na], x[na], ...)$y
+		y
+	}
+
+        y[] <- if (length(dim(y)) == 0)
+                na.approx.0(y)
+        else
+                apply(x, 2, na.approx.0)
+        if (na.rm) na.omit(y) else y
+}
+
