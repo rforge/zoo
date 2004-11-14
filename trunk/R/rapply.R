c093e85b57b426cc2b87ab7cf08d565ff5353b9e
ggrothendieck <ggrothendieck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2004-11-14 16:29:38 +0000

    Added rapply.R and rapply.Rd which generalize rollmean, rollmax and rollmed calling those in the special case that FUN is mean, max or median. We should remove rollmean.Rd although I have not removed it in this version.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/zoo@57 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/R/rapply.R
@@ -0,0 +1,26 @@
+
+rapply <- function(data, width, FUN, MARGIN = 1, by.column = TRUE, na.pad = TRUE, ...)
+	UseMethod("rapply")
+
+rapply.zoo <- function(data, width, FUN, MARGIN = 1, by.column = TRUE, na.pad = TRUE, ...) {
+    if (by.column) switch(deparse(substitute(FUN)),
+		mean = return(rollmean(data, width, na.pad = na.pad)),
+		max = return(rollmax(data, width, na.pad = na.pad)),
+		median = return(rollmed(data, width, na.pad = na.pad)))
+    nr <- NROW(data)
+    width <- as.integer(width)[1]
+    stopifnot( width > 0, width <= nr )
+    res <- if (!is.null(dim(data)) && by.column)
+	    apply( embed(1:nr, width), 1, 
+                function(st) apply(data[st,], 2, FUN, ...))
+    else
+	   apply( embed(1:nr, width), 1, 
+                function(st) FUN(data[st,], ...))
+    if (na.pad) { if (is.null(dim(res)))
+	zoo(c(rep(NA, width-1), res), index(data))
+    else
+	zoo(rbind(matrix(NA, nr=width-1, nc=ncol(res)), res), index(data))
+    } else
+	zoo(res, index(data)[-seq(len = width - 1)])
+} 
+
