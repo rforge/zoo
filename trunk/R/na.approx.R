46e5250ff2cd9c5b583524ed32d62dc41644d8cd
ggrothendieck <ggrothendieck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2004-11-16 16:49:12 +0000

    o Fixed na.approx   - seq now uses length= rather than along=   - interpolation was being done on index instead of value
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/zoo@64 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/R/na.approx.R
+++ trunk/R/na.approx.R
@@ -1,9 +1,10 @@
+
 na.approx <- function(y, x, FUN, na.rm = TRUE, ...) UseMethod("na.approx")
 
 # interpolates y using x which defaults to seq(along = y) if FUN
 # is missing or to FUN(time(y)) if not
 na.approx.default <- function(y, 
-	x = if (missing(FUN)) seq(along=time(y)) else FUN(time(y)), 
+	x = if (missing(FUN)) seq(len=length(time(y))) else FUN(time(y)), 
 	FUN, na.rm = TRUE, ...)
 {
 	# na.approx is based on post from r-help by 
@@ -11,7 +12,7 @@ na.approx.default <- function(y,
 	na.approx.0 <- function(y) {
 		na <- is.na(y)
 		if(all(!na)) return(y)
-		y[na] <- approx(x[!na], x[!na], x[na], ...)$y
+		y[na] <- approx(x[!na], y[!na], x[na], ...)$y
 		y
 	}
 
