f9916fd0715052e455bd27305ec8a4b75888faeb
ggrothendieck <ggrothendieck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2005-03-10 21:55:36 +0000

    as.ts.zoo, predict.AsIs, yearmon & defaultfrequency
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/zoo@97 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/R/as.zoo.R
+++ trunk/R/as.zoo.R
@@ -92,3 +92,18 @@ as.list.ts <- function(x, ...) {
 	else
 		list(x)
 }
+
+defaultfrequency <- function(x) UseMethod("defaultfrequency")
+defaultfrequency.Date <- function(x) 1
+defaultfrequency.yearmon <- function(x) 12
+defaultfrequency.default <- function(x) round(1/min(diff(as.numeric(x))))
+
+as.ts.zoo <- function(x, start = as.numeric(time(x[1])), 
+		frequency = defaultfrequency(time(x))) {
+	tt <- round(as.numeric(time(x)) * frequency)
+	stopifnot(!any(duplicated(tt)))
+	time(x) <- tt
+	x <- merge(x, zoo(, seq(round(start * frequency), tail(tt,1))))
+	ts(coredata(x), start = start, frequency = frequency)
+}
+
---
+++ trunk/R/yearmon.R
@@ -0,0 +1,39 @@
+
+as.yearmon <- function(x) UseMethod("as.yearmon")
+as.yearmon.numeric <- function(x, unit = "year") switch(unit,
+	year = structure(x, class = "yearmon"),
+	month = structure(x/12, class = "yearmon"))
+as.yearmon.integer <- function(x, unit = "year") switch(unit,
+	year = structure(x, class = "yearmon"),
+	month = structure(x/12, class = "yearmon"))
+as.yearmon.default <- function(x) 
+  structure( with(as.POSIXlt(x,tz="GMT"), 1900+year+mon/12), 
+	class = "yearmon")
+as.Date.yearmon <- function(x, ...) {
+	x <- unclass(x)
+	year <- floor(x + .001)
+	month <- floor(12 * (x - year) + 1 + .5 + .001)
+	as.Date(paste(year, month, 1, sep = "-"))
+}
+format.yearmon <- function (x, format = "%b %Y", ...) 
+{
+    xx <- format(as.Date(x), format = format, ...)
+    names(xx) <- names(x)
+    xx
+}
+as.character.yearmon <- function(x) format(x)
+print.yearmon <- function(x, ...) { 
+	print(format(x), ...)
+	invisible(x) 
+}
+"[.yearmon" <- function (x, ..., drop = TRUE) 
+{
+    cl <- oldClass(x)
+    class(x) <- NULL
+    val <- NextMethod("[")
+    class(val) <- cl
+    val
+}
+axis.yearmon <- function (side, x, at, format, ...) 
+	axis.Date(side, as.Date(x), at, format, ...)
+
