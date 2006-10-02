cf0958498cd5921da28ce1c6ee17d690853e7c48
zeileis <zeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2006-10-02 17:18:49 +0000

    improved stinepack dependencies
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/zoo@318 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/R/na.stinterp.R
+++ trunk/R/na.stinterp.R
@@ -4,16 +4,16 @@ na.stinterp <- function(object, ...) UseMethod("na.stinterp")
 # along has to be numeric, is otherwise coerced
 na.stinterp.default <- function(object, along = index(object), na.rm = TRUE, ...)
 {
-	require(stinepack)
-	along <- as.numeric(along)
-	na.stinterp.0 <- function(y) {
-		na <- is.na(y)
-		if(all(!na)) return(y)
-		y[na] <- stinterp(along[!na], as.numeric(y[!na]), along[na], ...)$y
-		return(y)
-	}
+  stopifnot(require("stinepack"))
+  along <- as.numeric(along)
+  na.stinterp.0 <- function(y) {
+    na <- is.na(y)
+    if(all(!na)) return(y)
+    y[na] <- stinterp(along[!na], as.numeric(y[!na]), along[na], ...)$y
+    return(y)
+  }
 
-        object[] <- if (length(dim(object)) == 0) na.stinterp.0(object)
-        	else apply(object, 2, na.stinterp.0)
-        if (na.rm) na.omit(object) else object
+  object[] <- if (length(dim(object)) == 0) na.stinterp.0(object)
+    else apply(object, 2, na.stinterp.0)
+  if(na.rm) na.omit(object) else object
 }
