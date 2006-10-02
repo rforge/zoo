9be679e12d0baa6d04a555f2bbaee33d56fc4fe2
ggrothendieck <ggrothendieck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2006-10-02 10:46:58 +0000

    Added Stineman interpolation routine na.stinterp and na.stinterp.default.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/zoo@317 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/R/na.stinterp.R
@@ -0,0 +1,19 @@
+na.stinterp <- function(object, ...) UseMethod("na.stinterp")
+
+# interpolates object along along which defaults to index(object)
+# along has to be numeric, is otherwise coerced
+na.stinterp.default <- function(object, along = index(object), na.rm = TRUE, ...)
+{
+	require(stinepack)
+	along <- as.numeric(along)
+	na.stinterp.0 <- function(y) {
+		na <- is.na(y)
+		if(all(!na)) return(y)
+		y[na] <- stinterp(along[!na], as.numeric(y[!na]), along[na], ...)$y
+		return(y)
+	}
+
+        object[] <- if (length(dim(object)) == 0) na.stinterp.0(object)
+        	else apply(object, 2, na.stinterp.0)
+        if (na.rm) na.omit(object) else object
+}
